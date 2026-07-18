98a502ba597d3027bc5e1850f3ed06b440b5dd5b
YifePlayte <62252171+YifePlayte@users.noreply.github.com>
2026-07-18 16:14:27 +0800

    1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.yifeplayte.norelaunch"
         minSdk = 34
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         applicationVariants.configureEach {
             outputs.configureEach {
